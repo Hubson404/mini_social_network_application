9523fb85611a1886382b7123e18cd29dd2eae43f
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-08-16 23:22:56 +0200

    Update AppSession class, add searchByUserName command.

--- src/main/java/org/hubson404/miniSocialNetwork/clients/AppSession.java
+++ src/main/java/org/hubson404/miniSocialNetwork/clients/AppSession.java
@@ -1,22 +1,32 @@
 package org.hubson404.miniSocialNetwork.clients;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hubson404.miniSocialNetwork.database.EntityDao;
+import org.hubson404.miniSocialNetwork.database.ServiceUserDao;
 import org.hubson404.miniSocialNetwork.model.Post;
 import org.hubson404.miniSocialNetwork.model.ServiceUser;
 
+import java.util.Optional;
 import java.util.Scanner;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppSession {
 
     private ServiceUser loggedUser;
+    private ServiceUser foundUser;
     private boolean isLoggedIn = false;
-    private String greeting = "Welcome to the MiniSocialNetwork.";
-    private String spacer = "=================================";
-    private EntityDao<Post> postEntityDao = new EntityDao<>();
-    private EntityDao<ServiceUser> userEntityDao = new EntityDao<>();
 
+    private final EntityDao<Post> peDao = new EntityDao<>();
+    private final ServiceUserDao suDao = new ServiceUserDao();
 
     public void openSession(Scanner scanner) {
+        String greeting = "Welcome to the MiniSocialNetwork.";
+        String spacer = "=================================";
+
         System.out.println(spacer + "\n" + greeting + "\n" + spacer);
         LoggingInClient loggingInClient = new LoggingInClient();
         String command;
@@ -26,29 +36,45 @@ public class AppSession {
             command = scanner.nextLine();
             switch (command) {
                 case "1":
-                    loggedUser = loggingInClient.logIn(scanner).get();
-                    isLoggedIn = true;
+                    Optional<ServiceUser> op;
+                    do {
+                        op = loggingInClient.logIn(scanner);
+                        op.ifPresent(this::setLoggedUser);
+                    } while (op.isEmpty());
+                    System.out.println("Welcome <" + loggedUser.getUserName() + ">");
+                    setLoggedIn(true);
                     break;
                 case "2":
-                    userEntityDao.saveOrUpdate(SignInClient.createAccount(scanner));
+                    suDao.saveOrUpdate(SignInClient.createAccount(scanner));
                     break;
-
                 default:
                     System.out.println("Command unknown.");
                     break;
             }
-        }while (!isLoggedIn);
+        } while (!isLoggedIn);
 
         do {
-            System.out.println("Select command: \n1) CREATE POST\n2) LOG OUT");
+            System.out.println("Select command: \n1) CREATE POST\n2) FIND USER BY USER_NAME\n3) LOG OUT");
             command = scanner.nextLine();
 
             switch (command) {
                 case "1":
                     Post post = PostingClient.writePost(scanner, loggedUser);
-                    postEntityDao.saveOrUpdate(post);
+                    peDao.saveOrUpdate(post);
                     break;
+
                 case "2":
+                    System.out.println("Insert userName of searched user: ");
+                    String userName;
+                    Optional<ServiceUser> op;
+
+                    userName = scanner.nextLine();
+                        op = suDao.findByUserName(ServiceUser.class, userName);
+                        op.ifPresent(this::setFoundUser);
+                        System.out.println(foundUser);
+                    break;
+
+                case "3":
                     System.out.println("Logging out. See you soon!");
                     loggedUser = null;
                     isLoggedIn = false;
@@ -58,9 +84,6 @@ public class AppSession {
                     break;
             }
         } while (isLoggedIn);
-
-
     }
-
-
 }
+
